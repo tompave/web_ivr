87596d2430cabd1e89c38dfe20b80a7aaffcd62d
tom <git@wonderingmachine.com>
2012-10-23 22:44:13 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Webivr::Application.config.secret_token = '31f396e43ced480f1832c238274ad41e27dd6c23a8e66159d7d369dcc764cc421d97a8faf5294456460bc4d58757ca63245715a42af8067b1cb997369f01fca1'
